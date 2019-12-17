565f389e39d34d52887354eff75f1fdb7363efb0
Zach <andrewsz@allegheny.edu>
2019-12-16 22:49:56 -0500

    New Jekyll Site

---
+++ assets/js/site.js
@@ -0,0 +1,10 @@
+// TypeWriter Animation
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 25,
+  backSpeed: 25,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: false,
+  showCursor: false
+});
